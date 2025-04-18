ec7eb9445f1200f0c6ad5e6ab4e258d722b03de6
Artur Rosin <arturrossinz@gmail.com>
2025-04-17 21:51:10 -0300

    Add files via upload
    Arquivo final.

--- crud.py
+++ crud.py
@@ -5,8 +5,8 @@ def carregar_pets():
     for item in tree.get_children():
         tree.delete(item)
     for pet in pets:
-        tree.insert('', 'end', values=(pet['id'],
-                                        pet['tutor'], pet['nome'], pet['especie'], pet['raca'], pet['idade'],))
+        tree.insert('', 'end', values=(pet['ID'],
+                                       pet['Tutor'], pet['Nome'], pet['Espécie'], pet['Raça'], pet['Idade'],))
 
 def adicionar_pet():
     global next_pet_id
@@ -26,20 +26,84 @@ def adicionar_pet():
                              'Idade deve ser um número inteiro!')
         return
     
-    novo_pet = { 'id': next_pet_id, 'tutor': tutor, 'nome': nome, 'especie': especie, 'raca': raca, 'idade': idade_int}
+    novo_pet = { 'ID': next_pet_id, 'Tutor': tutor, 'Nome': nome, 'Espécie': especie, 'Raça': raca, 'Idade': idade_int}
 
     pets.append(novo_pet)
     next_pet_id += 1 
 
     messagebox.showinfo('Sucesso', 'Pet cadastro com sucesso!')
-    #Limpar_campos
+    limpar_campos
     carregar_pets()
 
+def selecionar_pet(event):
+    selected_item = tree.selection()
+    if not selected_item:
+        return
+    
+    values = tree.item(selected_item)['values']
+    limpar_campos()
+
+    entry_tutor.insert(0,values[1])
+    entry_nome.insert(0,values[2])
+    entry_especie.insert(0,values[3])
+    entry_raca.insert(0,values[4])
+    entry_idade.insert(0, str(values[5]))
+
+def limpar_campos():
+    entry_tutor.delete(0, 'end')
+    entry_nome.delete(0, 'end')
+    entry_especie.delete(0, 'end')
+    entry_raca.delete(0, 'end')
+    entry_idade.delete(0, 'end')
+
+def editar_pet():
+    selected_item = tree.selection()
+    if not selected_item:
+        messagebox.showerror('Erro', 'Selecione um pet para editar!')
+        return
+    pet_id = tree.item(selected_item)['values'][0]
+    tutor = entry_tutor.get()
+    nome = entry_nome.get()
+    especie = entry_especie.get()
+    raca = entry_raca.get()
+    idade = entry_idade.get()
+
+    if not tutor or not nome:
+        messagebox.showerror('Erro', 'Tutor e nome do pet são obrigatórios!')
+        return
+    
+    try:
+        idade_int = int(idade) if idade else 0
+    except ValueError:
+        messagebox.showerror('Erro', 'Idade deve ser um número inteiro!')
+        return
+    for pet in pets:
+        if pet['ID'] == pet_id:
+            pet.update({'Tutor': tutor, 'Nome': nome, 'Espécie': especie, 'Raca': raca, 'Idade': idade_int})
+            break
+    messagebox.showinfo('Sucesso', 'Pet atualizado com sucesso!')
+    carregar_pets()
+                             
+def remover_pet():
+    selected_item = tree.selection()
+    if not selected_item:
+        messagebox.showerror('Erro', 'Selecione um pet para remover!')
+        return
+    
+    pet_id = tree.item(selected_item)['values'][0]
+
+    if messagebox.askyesno('Confirmação', 'Tem certeja que deseja remover este pet?'):
+        global pets
+        pets = [pet for pet in pets if pet['ID'] != pet_id]
+        messagebox.showinfo('Sucesso', 'Pet removido com sucesso!')
+        limpar_campos()
+        carregar_pets()
+     
 
 
 # Dados em memória
 pets = []
-nest_pet_id = 1
+next_pet_id = 1
 
 # Configuração da janela principal
 root = tk.Tk()
@@ -89,13 +153,13 @@ frame_botoes.pack(pady=5)
 btn_adicionar = ttk.Button(frame_botoes, text='Adicionar', command=adicionar_pet)
 btn_adicionar.grid(row=0, column=0, padx=5)
 
-btn_adicionar = ttk.Button(frame_botoes, text='Editar', comand=None)
+btn_adicionar = ttk.Button(frame_botoes, text='Editar', command=editar_pet)
 btn_adicionar.grid(row=0, column=1, padx=5)
 
-btn_adicionar = ttk.Button(frame_botoes, text='Remover', comand=None)
+btn_adicionar = ttk.Button(frame_botoes, text='Remover', command=remover_pet)
 btn_adicionar.grid(row=0, column=2, padx=5)
 
-btn_adicionar = ttk.Button(frame_botoes, text='Limpar', comand=None)
+btn_adicionar = ttk.Button(frame_botoes, text='Limpar', command=limpar_campos)
 btn_adicionar.grid(row=0, column=3, padx=5)
 
 # Tabela de pets
@@ -123,7 +187,7 @@ tree.configure(yscrollcommand=scrollbar.set)
 tree.pack(side='left',fill='both',expand= True)
 scrollbar.pack(side='right', fill='y')
 
-tree.bind('<<TreeviewSelect', None) #o bind fica esperando acontecer alguma coisa.
+tree.bind('<<TreeviewSelect>>', selecionar_pet) #o bind fica esperando acontecer alguma coisa.
 
 
 root. mainloop()
