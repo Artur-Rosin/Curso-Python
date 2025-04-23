0395cde8bd27b7a92e25c1d3eb548a96b2ea8bc9
Artur Rosin <arturrossinz@gmail.com>
2025-04-22 21:37:39 -0300

    Arquivo finalizado.

--- crud.py
+++ crud.py
@@ -1,10 +1,33 @@
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import ttk, messagebox, filedialog
+import json
 
-def carregar_pets():
+def salvar_para_json():
+    if not pets:
+        messagebox.showwarning('Aviso', 'Não há pets!')
+        return
+    #Abre a janela para selecionar onde salvar o arquivo.
+    arquivo = filedialog.asksaveasfilename(defaultextension='json', filetypes=[('Arquivo JSON', '*.json')], 
+                                           title='Salvar lista de pets como JSON')
+    if not arquivo: #Se o usuário cancelar
+        return
+    
+    try:
+        with open(arquivo, 'w', encoding='utf-8') as f:
+            json. dump(pets, f, ensure_ascii=False,
+                       indent=4)
+            messagebox.showinfo('Sucesso', f'Dados salvos com suscesso em:')
+    
+    except Exception as e:
+        messagebox.showerror('Erro', f"Ocorreu um erro ao salvar:\n{str(e)}")
+
+def carregar_pets(pets_list=None):
     for item in tree.get_children():
         tree.delete(item)
-    for pet in pets:
+
+    pets_to_load = pets_list if pets_list is not None else pets
+
+    for pet in pets_to_load:
         tree.insert('', 'end', values=(pet['ID'],
                                        pet['Tutor'], pet['Nome'], pet['Espécie'], pet['Raça'], pet['Idade'],))
 
@@ -99,7 +122,20 @@ def remover_pet():
         limpar_campos()
         carregar_pets()
      
+def pesquisar_por_tutor():
+    termo_pesquisar = entry_tutor.get().lower()
 
+    if not termo_pesquisar:
+        carregar_pets()
+        return
+    
+    pets_encontrados = [pet for pet in pets
+                        if termo_pesquisar in pet['Tutor'].lower()]
+    if not pets_encontrados:
+        messagebox.showinfo('Pesquisa', 'Nenhum pet encontrado para este tutor')
+        carregar_pets()
+    else:
+        carregar_pets(pets_encontrados)
 
 # Dados em memória
 pets = []
@@ -161,6 +197,13 @@ btn_adicionar.grid(row=0, column=2, padx=5)
 
 btn_adicionar = ttk.Button(frame_botoes, text='Limpar', command=limpar_campos)
 btn_adicionar.grid(row=0, column=3, padx=5)
+
+btn_adicionar = ttk.Button(frame_botoes, text='Pesquisar Tutor', command=pesquisar_por_tutor)
+btn_adicionar.grid(row=0, column=4, padx=5)
+
+btn_salvar_json = ttk.Button(frame_botoes, text='Salvar Json', command=salvar_para_json)
+btn_salvar_json.grid(row=0, column=5, padx=5)
+
 
 # Tabela de pets
 frame_tabela = ttk.Frame(root)
