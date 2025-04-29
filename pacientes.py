fde798d1c0d1a7f181e7311e427918cd7758cdbc
Artur Rosin <arturrossinz@gmail.com>
2025-04-29 20:12:41 -0300

    Cadastro de Pacientes.

--- pacientes.py
+++ pacientes.py
@@ -3,7 +3,7 @@ from tkinter import ttk, messagebox, filedialog
 import json
 
 def carregar_de_json():
-    global pacientes, next_nome_id
+    global s, next_paciente_id
 
     arquivo = filedialog.askopenfilename(filetypes=[('Arquivos JSON', '*.json')],
                                          title='Selecionar arquivo JSON para carregar')
@@ -12,17 +12,17 @@ def carregar_de_json():
         return
     try:
         with open(arquivo, 'r', encoding= 'utf-8') as f:
-            pacientes_carregar = json.load(f)
+            paciente_carregar = json.load(f)
 
-        # Atualiza a lista de pacientes e o próximo ID
-        pacientes = pacientes_carregar
-        if pacientes:
-            next_nome_id=max(nome['ID']
-                            for nome in pacientes)+1
+        # Atualiza a lista de paciente e o próximo ID
+        paciente = paciente_carregar
+        if paciente:
+            next_paciente_id=max(paciente['ID']
+                            for paciente in paciente)+1
         else:
-            next_nome_id = 1
+            next_paciente_id = 1
 
-        carregar_pacientes()
+        carregar_paciente()
 
         messagebox.showinfo('Sucesso', f'Dados carregados com sucesso de: \n{arquivo}')
     except Exception as e:
@@ -30,36 +30,36 @@ def carregar_de_json():
 
 
 def salvar_para_json():
-    if not pacientes:
-        messagebox.showwarning('Aviso', 'Não há pacientes!')
+    if not paciente:
+        messagebox.showwarning('Aviso', 'Não há paciente!')
         return
     
         #Abre a janela para selecionar onde salvar o arquivo.
     arquivo = filedialog.asksaveasfilename(defaultextension='.json', filetypes=[('Arquivo JSON', '*.json')], 
-                                           title='Salvar lista de pacientes como JSON')
+                                           title='Salvar lista de paciente como JSON')
     if not arquivo: #Se o usuário cancelar
         return
     
     try:
         with open(arquivo, 'w', encoding='utf-8') as f:
-            json.dump(pacientes, f, ensure_ascii=False, indent=4)
+            json.dump(paciente, f, ensure_ascii=False, indent=4)
             messagebox.showinfo('Sucesso', f'Dados salvos com suscesso em:')
     
     except Exception as e:
         messagebox.showerror('Erro', f"Ocorreu um erro ao salvar:\n{str(e)}")
 
-def carregar_pacientes(pacientes_list=None):
+def carregar_paciente(paciente_list=None):
     for item in tree.get_children():
         tree.delete(item)
 
-    pacientes_to_load = pacientes_list if pacientes_list is not None else pacientes
+    paciente_to_load = paciente_list if paciente_list is not None else paciente
 
-    for nome in pacientes_to_load:
-        tree.insert('', 'end', values=(nome['ID'],
-                                       nome['Nome'], nome['Data de nascimento'], nome['Gênero'], nome['Estado civil'], nome['Profissão'], nome['Celular'],))
+    for paciente in paciente_to_load:
+        tree.insert('', 'end', values=(paciente['ID'],
+                                       paciente['Nome'], paciente['Data de nascimento'], paciente['Gênero'], paciente['Estado civil'], paciente['Profissão'], paciente['Celular'],))
 
-def adicionar_nome():
-    global next_nome_id
+def adicionar_paciente():
+    global next_paciente_id
     nome = entry_nome.get()
     data_de_nascimento = entry_data_de_nascimento.get()
     gênero = entry_genero.get()
@@ -67,8 +67,8 @@ def adicionar_nome():
     profissao = entry_profissao.get()
     celular = entry_celular.get()
 
-    if not nome or not nome:
-        messagebox.showerror('Erro', 'Nome e nome de nome são obrigatórios!')
+    if not paciente or not paciente:
+        messagebox.showerror('Erro', 'Nome do paciente é obrigatórios!')
         return
     try:
         profissao_int = int(profissao) if profissao else 0 
@@ -77,16 +77,16 @@ def adicionar_nome():
                              'Profissao não deve conter número!')
         return
     
-    novo_nome = { 'ID': next_nome_id, 'Nome': nome, 'Data de nascimento': data_de_nascimento, 'Gênero': gênero, 'Estado civil': estado_civil, 'Profissao': profissao, 'Celular': celular_int}
+    novo_paciente = { 'ID': next_paciente_id, 'Nome': nome, 'Data de nascimento': data_de_nascimento, 'Gênero': gênero, 'Estado civil': estado_civil, 'Profissao': profissao, 'Celular': celular_int}
 
-    pacientes.append(novo_nome)
-    next_nome_id += 1 
+    paciente.append(novo_paciente)
+    next_paciente_id += 1 
 
-    messagebox.showinfo('Sucesso', 'pacientes cadastro com sucesso!')
+    messagebox.showinfo('Sucesso', 'Paciente cadastro com sucesso!')
     limpar_campos
-    carregar_pacientes()
+    carregar_paciente()
 
-def selecionar_nome(event):
+def selecionar_paciente(event):
     selected_item = tree.selection()
     if not selected_item:
         return
@@ -96,34 +96,34 @@ def selecionar_nome(event):
 
     entry_nome.insert(0,values[1])
     entry_data_de_nascimento.insert(0,values[2])
-    entry_genero.insert(0,values[3])
+    entry_genero_de_nascimento.insert(0,values[3])
     entry_estado_civil.insert(0,values[4])
     entry_profissao.insert(0,values[5])
-    entry_celular.insert(0, str(values[6])
+    entry_celular.insert(0, str(values[6]))
 
 def limpar_campos():
     entry_nome.delete(0, 'end')
     entry_data_de_nascimento.delete(0, 'end')
-    entry_genero.delete(0, 'end')
+    entry_genero_de_nascimento.delete(0, 'end')
     entry_estado_civil.delete(0, 'end')
     entry_profissao.delete(0, 'end')
     entry_celular.delete(0, 'end')
 
-def editar_nome():
+def editar_paciente():
     selected_item = tree.selection()
     if not selected_item:
-        messagebox.showerror('Erro', 'Selecione um nome para editar!')
+        messagebox.showerror('Erro', 'Selecione um paciente para editar!')
         return
-    nome_id = tree.item(selected_item)['values'][0]
-    nome = entry_nome.get()
+    paciente_id = tree.item(selected_item)['values'][0]
+    paciente = entry_paciente.get()
     data_de_nascimento = entry_data_de_nascimento.get()
-    gênero = entry_gênero.get()
+    gênero = entry_gênero_de_nascimento.get()
     estado_civil = entry_estado_civil.get()
     profissao = entry_profissao.get()
     celular = entry_celular.get()
 
-    if not nome or not nome:
-        messagebox.showerror('Erro', 'Nome e nome do nome são obrigatórios!')
+    if not paciente or not paciente:
+        messagebox.showerror('Erro', 'Nome do paciente é obrigatórios!')
         return
     
     try:
@@ -131,46 +131,46 @@ def editar_nome():
     except ValueError:
         messagebox.showerror('Erro', 'Profissao não deve conter número!')
         return
-    for nome in pacientes:
-        if nome['ID'] == nome_id:
-            nome.update({'Nome': nome, '': data_de_nascimento, 'Data de nascimento': gênero, 'estado_civil': estado_civil, 'Profissao': profissao, 'Celular': celular_int})
+    for paciente in paciente:
+        if paciente['ID'] == paciente_id:
+            paciente.update({'Paciente': paciente, '': data_de_nascimento, 'Data de nascimento': gênero, 'estado_civil': estado_civil, 'Profissao': profissao, 'Celular': celular_int})
             break
-    messagebox.showinfo('Sucesso', 'Nome atualizado com sucesso!')
-    carregar_pacientes()
+    messagebox.showinfo('Sucesso', 'Paciente atualizado com sucesso!')
+    carregar_paciente()
                              
-def remover_nomet():
+def remover_paciente():
     selected_item = tree.selection()
     if not selected_item:
-        messagebox.showerror('Erro', 'Selecione um nome para remover!')
+        messagebox.showerror('Erro', 'Selecione um paciente para remover!')
         return
     
-    nome_id = tree.item(selected_item)['values'][0]
+    paciente_id = tree.item(selected_item)['values'][0]
 
-    if messagebox.askyesno('Confirmação', 'Tem certeja que deseja remover este nome?'):
-        global pacientes
-        pacientes = [nome for nome in pacientes if nome['ID'] != nome_id]
-        messagebox.showinfo('Sucesso', 'Nome removido com sucesso!')
+    if messagebox.askyesno('Confirmação', 'Tem certeja que deseja remover este paciente?'):
+        global paciente
+        paciente = [paciente for paciente in paciente if paciente['ID'] != paciente_id]
+        messagebox.showinfo('Sucesso', 'Paciente removido com sucesso!')
         limpar_campos()
-        carregar_pacientes()
+        carregar_paciente()
      
-def pesquisar_por_nome():
-    termo_pesquisar = entry_nome.get().lower()
+def pesquisar_por_paciente():
+    termo_pesquisar = entry_paciente.get().lower()
 
     if not termo_pesquisar:
-        carregar_pacientes()
+        carregar_paciente()
         return
     
-    pacientes_encontrados = [nome for nome in pacientes
-                        if termo_pesquisar in nome['Nome'].lower()]
-    if not pacientes_encontrados:
-        messagebox.showinfo('Pesquisa', 'Nenhum nome encontrado para este nome')
-        carregar_pacientes()
+    paciente_encontrados = [paciente for paciente in paciente
+                        if termo_pesquisar in paciente['Paciente'].lower()]
+    if not paciente_encontrados:
+        messagebox.showinfo('Pesquisa', 'Nenhum paciente encontrado para este nome')
+        carregar_paciente()
     else:
-        carregar_pacientes(pacientes_encontrados)
+        carregar_paciente(paciente_encontrados)
 
 # Dados em memória
-pacientes = []
-next_nome_id = 1
+paciente = []
+next_paciente_id = 1
 
 # Configuração da janela principal
 root = tk.Tk()
@@ -179,11 +179,11 @@ root.geometry('800x500')
 
 # Frame do formulário
 frame_form = ttk.LabelFrame(root,
-                            text='Pacientes')
+                            text='pacientes')
 frame_form.pack(padx=10, pady=5, fill='x')
 
 # Campos do formulário
-ttk.Label(frame_form, text='Nome:').grid(row=0,
+ttk.Label(frame_form, text='Paciente:').grid(row=0,
                                           column=0, padx=5,pady=5,
                                         sticky='e')
 entry_nome = ttk.Entry(frame_form,width=40)
@@ -195,47 +195,66 @@ ttk.Label(frame_form, text='Data de nascimento:').grid(row=1,
 entry_data_de_nascimento = ttk.Entry(frame_form,width=40)
 entry_data_de_nascimento.grid(row=1, column=1, padx=5, pady=5)
 
-ttk.Label(frame_form, text='Gênero:').grid(row=2,
-                                          column=0, padx=5, pady=5,
-                                        sticky='e')
-entry_gênero = ttk.Entry(frame_form,width=40)
-entry_gênero.grid(row=2, column=1, padx=5, pady=5)
-
-ttk.Label(frame_form, text='Estado civil:').grid(row=3,
+ttk.Label(frame_form, text='Estado civil:').grid(row=2,
                                           column=0, padx=5, pady=5,
                                         sticky='e')
 entry_estado_civil = ttk.Entry(frame_form,width=40)
-entry_estado_civil.grid(row=3, column=1, padx=5, pady=5)
+entry_estado_civil.grid(row=2, column=1, padx=5, pady=5)
 
-ttk.Label(frame_form, text='Profissão:').grid(row=4,
+ttk.Label(frame_form, text='Profissão:').grid(row=3,
                                           column=0, padx=5, pady=5,
                                         sticky='e')
 entry_profissao = ttk.Entry(frame_form,width=40)
-entry_profissao.grid(row=4, column=1, padx=5, pady=5)
+entry_profissao.grid(row=3, column=1, padx=5, pady=5)
 
-ttk.Label(frame_form, text='celular:').grid(row=5,
+ttk.Label(frame_form, text='celular:').grid(row=4,
                                           column=0, padx=5, pady=5,
                                         sticky='e')
 entry_celular = ttk.Entry(frame_form,width=40)
-entry_celular.grid(row=5, column=1, padx=5, pady=5)
+entry_celular.grid(row=4, column=1, padx=5, pady=5)
+
+# Radio Buttons para Gênero  
+tk.Label(frame_form, text='Genero de nascimento:').grid(row=1,
+                                          column=3, padx=5, pady=5,
+                                          sticky='e')
+
+opcoes =[  
+        ("Feminino", "Feminino"),  
+        ("Masculino", "Masculino")] 
+
+genero_selecionado = tk.StringVar(value="None")  # Valor padrão  
+
+for i, (texto, valor) in enumerate(opcoes):  # Adicione 'enumerate' aqui!
+    tk.Radiobutton(
+        frame_form, 
+        text=texto, 
+        variable=genero_selecionado, 
+        value=valor
+    ).grid(
+        row=2 + i,  # Agora 'i' está definido pelo enumerate
+        column=3, 
+        padx=5, 
+        pady=5, 
+        sticky='e'
+    )
 
 # Frame de botões
 frame_botoes = ttk.Frame(root)
 frame_botoes.pack(pady=5)
 
-btn_adicionar = ttk.Button(frame_botoes, text='Adicionar', command=adicionar_nome)
+btn_adicionar = ttk.Button(frame_botoes, text='Adicionar', command=adicionar_paciente)
 btn_adicionar.grid(row=0, column=0, padx=5)
 
-btn_adicionar = ttk.Button(frame_botoes, text='Editar', command=editar_nome)
+btn_adicionar = ttk.Button(frame_botoes, text='Editar', command=editar_paciente)
 btn_adicionar.grid(row=0, column=1, padx=5)
 
-btn_adicionar = ttk.Button(frame_botoes, text='Remover', command=remover_nome)
+btn_adicionar = ttk.Button(frame_botoes, text='Remover', command=remover_paciente)
 btn_adicionar.grid(row=0, column=2, padx=5)
 
 btn_adicionar = ttk.Button(frame_botoes, text='Limpar', command=limpar_campos)
 btn_adicionar.grid(row=0, column=3, padx=5)
 
-btn_adicionar = ttk.Button(frame_botoes, text='Pesquisar Pacientes ', command=pesquisar_por_nome)
+btn_adicionar = ttk.Button(frame_botoes, text='Pesquisar Pacientes ', command=pesquisar_por_paciente)
 btn_adicionar.grid(row=0, column=4, padx=5)
 
 btn_salvar_json = ttk.Button(frame_botoes, text='Salvar', command=salvar_para_json)
@@ -272,7 +291,7 @@ tree.configure(yscrollcommand=scrollbar.set)
 tree.pack(side='left',fill='both',expand= True)
 scrollbar.pack(side='right', fill='y')
 
-tree.bind('<<TreeviewSelect>>', selecionar_nome) #o bind fica esperando acontecer alguma coisa.
+tree.bind('<<TreeviewSelect>>', selecionar_paciente) #o bind fica esperando acontecer alguma coisa.
 
 
 root. mainloop()
